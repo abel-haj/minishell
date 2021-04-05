14fd725389a3de3b0166565395d16cf8e3f49a1b
Abdelaziz Bel haj <abel-haj@e1r2p8.1337.ma>
2021-04-05 18:43:20 +0100

    some tests

--- main.c
+++ main.c
@@ -1,15 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "minishell.h"
 
-int	main(int argc, char *argv[])
+size_t		ft_countsplit(const char *s, char c)
 {
-	char*	line;
+	size_t	i;
+	size_t	j;
 
-	while (get_next_line(0, &line) >= 0)
+	i = 0;
+	j = 0;
+	while (s[i])
 	{
-		printf("%s\n", line);
-		free(line);
+		while (s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+			i++;
+		if (s[i - 1] != c)
+			j++;
 	}
+	return (j);
+}
+
+// void		ft_treat_line(char *line)
+// {
+// 	char	**cmds;
+
+// 	// split commands
+// 	cmds = ft_split(line, ';');
+// 	// split pipes
+// 	printf("\n%d\n", ft_countsplit(line, c));
+// }
+
+int			main(int argc, char *argv[])
+{
+	char	*line;
+	char	*exargs[] = {"/", NULL};
+	// char	**exargs;
+	// **exargs = {"/", NULL};
+
+	execve("/bin/ls", exargs, NULL);
+	// while (get_next_line(0, &line) >= 0)
+	// {
+	// 	// printf("%s\n", line);
+	// 	ft_treat_line(line);
+	// 	free(line);
+	// }
 	return (0);
 }
