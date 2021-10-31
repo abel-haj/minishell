d482caba058bdcbeff7915040782ff478731432b
Abdelaziz Belhaj <abel-haj@e1r7p3.1337.ma>
2021-10-31 14:37:22 +0100

    norming splitter

--- parse/parse_utils_one.c
+++ parse/parse_utils_one.c
@@ -1,50 +1,71 @@
 #include "../minishell.h"
 
-static size_t	count_splitted_wq(char *s)
+int	is_quoted(char *qs)
 {
 	size_t	i;
 	int		quote;
 
-	i = -1;
 	quote = 0;
-	while (s[++i])
+	i = -1;
+	while (qs[++i])
 	{
-		if (s[i] == '\'' && quote == 0)
+		if (qs[i] == '\'' && quote == 0)
 			quote = 1;
-		else if (s[i] == '"' && quote == 0)
+		else if (qs[i] == '"' && quote == 0)
 			quote = 2;
-		else if (s[i] == '\'' == quote == 1)
+		else if (qs[i] == '\'' == quote == 1)
 			quote = 0;
-		else if (s[i] == '"' && quote == 2)
+		else if (qs[i] == '"' && quote == 2)
 			quote = 0;
-
-		if (s[i] == '|' && quote == 0)
-			quote++;
 	}
 	return (quote);
 }
 
-**	ft_split_wq(char *s, char c)
+static size_t	count_splitted_wq(char *s)
+{
+	size_t	i;
+	size_t	pipes;
+
+	i = -1;
+	pipes = 0;
+	while (s[++i])
+	{
+		if (s[i] == '|' && !is_quoted(s + i))
+			pipes++;
+	}
+	return (pipes);
+}
+
+char	**ft_split_wq(char *s, char c)
 {
 	size_t	start;
 	size_t	end;
 	size_t	i;
+	size_t	j;
 	size_t	len;
 	char	**result;
 
-	i = 0;
-	start = i;
-	end = start;
-	len	= count_splitted_wq(s);
-	result = malloc(sizeof(char *) * len + 1);
+	i = -1;
+	j = 0;
+	start = 0;
+	len = count_splitted_wq(s) + 1;
+	result = (char **)malloc(sizeof(char *) * (len + 1));
 	if (!result)
 		return (NULL);
-
-	while (result[++i])
+	while (s[++i] && j < len)
 	{
-		//
+		if (!is_quoted(s + i) && s[i] == '|')
+		{
+			end = i;
+			result[j] = ft_substr(s, start, i - start);
+			start = i + 1;
+			j++;
+		}
 	}
-
-	result[len - 1] = NULL;
+	if (len == 1)
+		result[j] = ft_substr(s, 0, i);
+	else
+		result[j] = ft_substr(s, end + 1, i - end);
+	result[len] = NULL;
 	return (result);
 }
